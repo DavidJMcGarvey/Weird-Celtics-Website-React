9746db2beb48ea4f16678c1ecb9af1b8b9728873
David McGarvey <davey@MacBook-spAir.local>
2020-03-04 20:35:52 -0800

    add context

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ import Home from './components/Home';
 import UserSignIn from './components/UserSignIn.js';
 import UserSignOut from './components/UserSignOut';
 import UserSignUp from './components/UserSignUp';
+import NotFound from './components/NotFound';
+import Forbidden from './components/Forbidden';
+import UnhandledError from './components/UnhandledError';
+import withContext from './Context';
+
+// Add context to components
+const HeaderWithContext = withContext(Header);
+const HomeWithContext = withContext(Home);
+
+const UserSignInWithContext = withContext(UserSignIn);
+const UserSignUpwithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
 
 
 export default class App extends Component {
@@ -25,16 +37,20 @@ export default class App extends Component {
     return (
       // Route handling
       <Router>
-        <Header />
+        <HeaderWithContext />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={HomeWithContext} />
           {/* <Route path="/smarf-gems" component={SmarfGems} />
           <Route path="/smarf-gems/:id" component={SmarfGemDetail} />
           <Route path="/twitter-talent" component={TwitterTalent} />
           <Route path="/twitter-talent/:id" component={TwitterTalentDetail} /> */}
-          <Route path="/signin" component={UserSignIn} />
-          <Route path="/signout" component={UserSignOut} />
-          <Route path="/signup" component={UserSignUp} />
+          <Route path="/signin" component={UserSignInWithContext} />
+          <Route path="/signout" component={UserSignOutWithContext} />
+          <Route path="/signup" component={UserSignUpwithContext} />
+          <Route path="/forbidden" component={Forbidden} />
+          <Route path="/error" component={UnhandledError} />
+          <Route path="/notfound" component={NotFound} />
+          <Route component={NotFound} />
         </Switch>
       </Router>
     )
